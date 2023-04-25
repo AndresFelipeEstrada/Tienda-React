052e46426902d475c6d7478719491f9dbf3dceb5
Andres <90068402+AndresFelipeEstrada@users.noreply.github.com>
2023-04-25 14:54:16 -0500

    login css to tailwind

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import '../styles/login.css'
+// import '../styles/login.css'
 import { Toaster, toast } from 'sonner'
 
 import { useCallback, useState } from 'react'
@@ -45,30 +45,42 @@ const Login = () => {
   }
 
   return (
-    <div className='Login'>
-      <div className='Login-container'>
-        <img src={logo} alt='logo' className='logo-login' />
-        <form className='form'>
-          <label htmlFor='email' className='label'>Email address</label>
-          <input type='text' defaultValue={magicWord.username} onChange={handleChangeUsername} placeholder='platzi@example.cm' className='input input-email' />
+    <>
+      <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+          <img
+            className='mx-auto h-10 w-auto'
+            src={logo}
+            alt='logo'
+          />
+          <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+            Ingresar a tu cuenta
+          </h2>
+        </div>
 
-          <label htmlFor='password' className='label'>Password</label>
-          <input type='password' defaultValue={magicWord.password} onChange={handleChangePassword} placeholder='*********' className='input input-password' />
-          {error && <p style={{ color: 'red' }}>{error}</p>}
-          <button
-            className='primary-button login-button'
-            onClick={handleSubmit}
-          >Log in
-          </button>
-        </form>
+        <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+          <form className='flex flex-col'>
+            <label htmlFor='email' className='text-sm font-bold mb-1'>Email address</label>
+            <input type='text' defaultValue={magicWord.username} onChange={handleChangeUsername} placeholder='platzi@example.cm' className='bg-gray-100 rounded-lg h-8 text-base p-1 mb-5' />
 
-        <Link to='/password-recovery' className='password-redirect'>Forgot my password</Link>
+            <label htmlFor='password' className='text-sm font-bold mb-1'>Password</label>
+            <input type='password' defaultValue={magicWord.password} onChange={handleChangePassword} placeholder='*********' className='bg-gray-100 rounded-lg h-8 text-base p-1 mb-5' />
+            {error && <p className='text-red-700'>{error}</p>}
+            <button
+              className='bg-hospital-green rounded-lg text-white w-full cursor-pointer text-base font-bold h-12 mt-3 mb-8'
+              onClick={handleSubmit}
+            >Log in
+            </button>
+          </form>
 
-        <Link to='/signup' className='secondary-button signup-button'>Sign up</Link>
+          <Link to='/password-recovery' className='text-hospital-green text-sm text-center no-underline mb-12'>Forgot my password</Link>
+
+          <Link to='/signup' className='bg-white rounded-lg text-hospital-green w-full cursor-pointer text-sm font-bold h-12 border-2 border-solid border-hospital-green no-underline flex items-center justify-center'>Sign up</Link>
+
+        </div>
       </div>
-
       <Toaster richColors />
-    </div>
+    </>
   )
 }
 
