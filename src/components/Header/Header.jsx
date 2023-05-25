536474737fe214bf43c2667c91caa74822a6f201
Andres <90068402+AndresFelipeEstrada@users.noreply.github.com>
2023-05-25 09:09:10 -0500

    add:wave

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,9 +14,9 @@ const Header = () => {
         <div className='container mx-auto'>
           <div className='flex items-center justify-between'>
 
-            <div>
+            <div className=''>
               <Link to='/'>
-                <img src={logo} alt='logo' />
+                <img className='h-12' src={logo} alt='logo' />
               </Link>
             </div>
 
@@ -29,25 +29,25 @@ const Header = () => {
               <div className='w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20' id='nav-content'>
                 <ul className='list-reset lg:flex justify-end flex-1 items-center'>
                   <li className='mr-3'>
-                    <Link className='inline-block py-2 px-4 text-black font-bold no-underline' to='/'>Clothes</Link>
+                    <Link className='inline-block py-2 px-4 text-black font-bold no-underline' to='/'>Ropa</Link>
                   </li>
                   <li className='mr-3'>
-                    <Link className='inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4' to='/'>Electronics</Link>
+                    <Link className='inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4' to='/'>Electronicos</Link>
                   </li>
                   <li className='mr-3'>
-                    <Link className='inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4' to='/'>Furnitures</Link>
+                    <Link className='inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4' to='/'>Muebles</Link>
                   </li>
                   <li className='mr-3'>
-                    <Link className='inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4' to='/'>Toys</Link>
+                    <Link className='inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4' to='/'>Juguetes</Link>
                   </li>
                   <li className='mr-3'>
-                    <Link className='inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4' to='/'>Others</Link>
+                    <Link className='inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4' to='/'>Otros</Link>
                   </li>
                 </ul>
               </div>
             </div>
 
-            <div>
+            <div className='hidden sm:block'>
               <button
                 id='navAction'
                 className='mx-auto lg:mx-0 justify-end hover:underline text-button-text bg-background-button font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out'
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 
 import { memo } from 'react'
 import fondo from '@logos/hero.png'
+import wave from '@icons/wave.svg'
 import { useNavigate } from 'react-router-dom'
 // import ProductList from '../containers/ProductList'
 
@@ -8,10 +9,8 @@ const Home = () => {
   const navigate = useNavigate()
   return (
     <>
-      <div className='bg_color' />
-      <div className='wave' />
 
-      <div className='pt-4'>
+      <div className='pt-4 bg-background-button'>
         <div className='container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center z-50'>
           <div className='flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left'>
             <p className='uppercase tracking-loose w-full text-paragraph'>¿Necesitas un profesional?</p>
@@ -30,8 +29,11 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <div>
+        <img src={wave} alt='' />
+      </div>
 
-      <section className='bg-white border-b py-8'>
+      <section className='bg-white border-b'>
         <div className='container max-w-5xl mx-auto m-8'>
           <h2 className='w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800'>
             Title
@@ -375,7 +377,7 @@ const Home = () => {
             <div className='flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6'>
               <div className='flex items-center justify-start'>
                 <button className='mx-auto lg:mx-0 hover:underline bg-background-button text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out'>
-                  Action
+                  Buscar
                 </button>
               </div>
             </div>
@@ -397,7 +399,7 @@ const Home = () => {
             <div className='flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6'>
               <div className='flex items-center justify-center'>
                 <button className='mx-auto lg:mx-0 hover:underline bg-background-button text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out'>
-                  Action
+                  Buscar
                 </button>
               </div>
             </div>
@@ -419,7 +421,7 @@ const Home = () => {
             <div className='flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6'>
               <div className='flex items-center justify-end'>
                 <button className='mx-auto lg:mx-0 hover:underline bg-background-button text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out'>
-                  Action
+                  Buscar
                 </button>
               </div>
             </div>
@@ -427,39 +429,41 @@ const Home = () => {
         </div>
       </section>
 
-      <svg className='wave-top' viewBox='0 0 1439 147' version='1.1'>
-        <g stroke='none' stroke-width='1' fill='none' fill-rule='evenodd'>
-          <g transform='translate(-1.000000, -14.000000)' fill-rule='nonzero'>
-            <g className='wave' fill='#f8fafc'>
-              <path
-                d='M1440,84 C1383.555,64.3 1342.555,51.3 1317,45 C1259.5,30.824 1206.707,25.526 1169,22 C1129.711,18.326 1044.426,18.475 980,22 C954.25,23.409 922.25,26.742 884,32 C845.122,37.787 818.455,42.121 804,45 C776.833,50.41 728.136,61.77 713,65 C660.023,76.309 621.544,87.729 584,94 C517.525,105.104 484.525,106.438 429,108 C379.49,106.484 342.823,104.484 319,102 C278.571,97.783 231.737,88.736 205,84 C154.629,75.076 86.296,57.743 0,32 L0,0 L1440,0 L1440,84 Z'
-              />
-            </g>
-            <g transform='translate(1.000000, 15.000000)' fill='#FFFFFF'>
-              <g transform='translate(719.500000, 68.500000) rotate(-180.000000) translate(-719.500000, -68.500000) '>
-                <path d='M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496' opacity='0.100000001' />
+      <div className='bg-background-button'>
+        <svg className='wave-top' viewBox='0 0 1439 147' version='1.1'>
+          <g stroke='none' stroke-width='1' fill='none' fill-rule='evenodd'>
+            <g transform='translate(-1.000000, -14.000000)' fill-rule='nonzero'>
+              <g className='wave' fill='#f8fafc'>
                 <path
-                  d='M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z'
-                  opacity='0.100000001'
+                  d='M1440,84 C1383.555,64.3 1342.555,51.3 1317,45 C1259.5,30.824 1206.707,25.526 1169,22 C1129.711,18.326 1044.426,18.475 980,22 C954.25,23.409 922.25,26.742 884,32 C845.122,37.787 818.455,42.121 804,45 C776.833,50.41 728.136,61.77 713,65 C660.023,76.309 621.544,87.729 584,94 C517.525,105.104 484.525,106.438 429,108 C379.49,106.484 342.823,104.484 319,102 C278.571,97.783 231.737,88.736 205,84 C154.629,75.076 86.296,57.743 0,32 L0,0 L1440,0 L1440,84 Z'
                 />
-                <path d='M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z' opacity='0.200000003' />
+              </g>
+              <g transform='translate(1.000000, 15.000000)' fill='#FFFFFF'>
+                <g transform='translate(719.500000, 68.500000) rotate(-180.000000) translate(-719.500000, -68.500000) '>
+                  <path d='M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496' opacity='0.100000001' />
+                  <path
+                    d='M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z'
+                    opacity='0.100000001'
+                  />
+                  <path d='M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z' opacity='0.200000003' />
+                </g>
               </g>
             </g>
           </g>
-        </g>
-      </svg>
+        </svg>
+      </div>
       <section className='container mx-auto text-center py-6 mb-12'>
         <h2 className='w-full my-2 text-5xl font-bold leading-tight text-center text-black'>
-          Call to Action
+          Buscar
         </h2>
         <div className='w-full mb-4'>
           <div className='h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t' />
         </div>
         <h3 className='my-4 text-3xl leading-tight text-black'>
-          Main Hero Message to sell yourself!
+          Lorem, ipsum.
         </h3>
         <button className='mx-auto lg:mx-0 hover:underline bg-background-button text-button-text font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out'>
-          Action!
+          Buscar
         </button>
       </section>
     </>
